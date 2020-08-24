b5852312d48cc5254e72c115719a9fb6a95f7187
Aaron Jamal <aaron.jamal09@gmail.com>
2020-08-24 03:25:03 -0400

    Updating before turning it in

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ var connection = mysql.createConnection({
 
     // Your password
     password: "Aj_112491",
-    database: "ice_creamDB"
+    database: "EmployeesDB"
 });
-
+//connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
@@ -23,9 +23,9 @@ connection.connect(function (err) {
 
 
     //ends connection to the database
-    connection.end();
+    //connection.end();
 });
-
+// This should bring up the main menu after the connection has been made
 function mainMenu() {
     inquirer
         .prompt({
@@ -62,6 +62,106 @@ function mainMenu() {
         })
 };
 
-//function addEmployee(){
-    //console.log("About to add an employee to the DB.")
-//}
+function addEmployee() {
+    console.log("Add new employee.")
+    console.log("--------------------------")
+    inquirer
+        .prompt([
+            {
+                name: "firstName",
+                type: "input",
+                message: "First Name? "
+            },
+
+            {
+                name: "lastName",
+                type: "input",
+                message: "Last Name?"
+            },
+
+            {
+                name: "department",
+                type: "list",
+                message: "Select a  Department:",
+                choices: ["IT", "Customer Sevice", "Sales", "Human Resources", "Claims"]
+            },
+
+            {
+                name: "role",
+                type: "list",
+                message: "Title? ",
+                choices: ["Salesperson", "Customer Service Rep", "Human Resources Rep", "Claims Agent", "IT Support", "Lead Engineer", "Software Developer"]
+            },
+
+            {
+                name: "manager",
+                type: "list",
+                message: "Manager?",
+                choices: ["Jermaine Cole", "Kendrick Lamar", "Nassir Jones", "Andre Benjamin"]
+            },
+        ])
+        .then(function (answer) {
+            console.log(answer.firstName)
+            console.log(answer.lastName)
+            console.log(answer.department)
+            connection.query(
+                "INSERT INTO employee SET ? ",
+                {
+                    first_name: answer.firstName,
+                    last_name: answer.lastName,
+                },
+                console.log("Name Added!"),
+                "INSERT INTO department SET ?",
+                {
+                    name: answer.department,
+                },
+                console.log("Department Added!"),
+                "INSERT INTO role SET ?",
+                {
+                    title: answer.role
+                },
+                console.log("Role Added!"),
+                function (err) {
+                    if (err) throw err;
+                    console.log("New employee has been added.");
+                    mainMenu()
+                }
+            )
+        })
+};
+
+
+function addRole() {
+    connection.query("SELECT * FROM employee", function (err, results) {
+        if (err) throw err;
+
+        inquirer
+            .prompt([
+                {
+                    name: "newRole",
+                    type: "input",
+                    message: "What is the new role going to be?"
+                },
+
+                {
+                    name: "salary",
+                    type: "list",
+                    choices: [50000, 70000, 90000],
+                    message: "What is the starting salary?"
+                }
+            ])
+            .then(function (answer) {
+                connection.query(
+                    "INSERT INTO role SET ?",
+                    [
+                        {
+                            title: answer.newRole,
+                            salary: answer.salary
+                        }
+                    ]
+                )
+            })
+    })
+}
+
+
